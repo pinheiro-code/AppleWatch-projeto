49eaa59bca34c2dd6070f131b82e0abd4991566b
pinheiro-code <arthurpinheiroaraujo06@gmail.com>
2026-01-21 11:14:27 -0300

    feat: atualiza tamanho do produto e ajusta visual dinamicamente

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // const imagemVisualizacao = document.getElementById("visualizacao");
 const imagemVisualizacao = document.querySelector("#visualizacao img");
+const tituloProduto = document.querySelector("h1");
+const opcoesTamanho = ["41 mm", "45 mm"];
+
+function atualizarTamanho() {
+  const opcaoTamanhoSelecionado = document
+    .querySelector('[name="opcao-tamanho"]:checked')
+    .id.charAt(0); // 0 ou 1 - 41 mm ou 45 mm
+
+  numTamanhoSelecionado = opcaoTamanhoSelecionado;
+
+  const tamanhoCaixa = opcoesTamanho[numTamanhoSelecionado];
+
+  console.log(tamanhoCaixa);
+
+  //   console.log(opcaoTamanhoSelecionado);
+
+  tituloProduto.innerText =
+    "Pulseira loop esportiva azul-inverno para caixa de " + tamanhoCaixa;
+
+  if (tamanhoCaixa === "41 mm") {
+    imagemVisualizacao.classList.add("caixa-pequena");
+  } else {
+    imagemVisualizacao.classList.remove("caixa-pequena");
+  }
+}
 
 function atualizarImagemSelecionada() {
   const opcaoImagemSelecionada = document
